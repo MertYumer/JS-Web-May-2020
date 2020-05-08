d43f7cd12a621c4860d926403bea7e8c4ca5a01e
Mert Yumer <mertyumer66@gmail.com>
2020-05-08 21:09:20 +0300

    Fixed bug in auth.js

--- JS Back-End - May 2020/Workshop - Session and Authentication/Cubicle/utils/auth.js
+++ JS Back-End - May 2020/Workshop - Session and Authentication/Cubicle/utils/auth.js
@@ -2,21 +2,46 @@ const env = process.env.NODE_ENV || 'development';
 const config = require('../config/config')[env];
 
 const jwt = require('./jwt');
-const usersService = require('../services/usersService');
+const context = require('../models/index');
 
 function auth() {
     return async function (req, res, next) {
         const token = req.cookies[config.authCookieName] || '';
 
-        const userData = await jwt
-            .verifyToken(token)
-            .catch(err => {
-                res.redirect('/');
+        Promise.all([
+            jwt.verifyToken(token),
+        ]).then(([data]) => {
+            context.users
+                .findById(data.id)
+                .then(user => {
+                    req.user = user;
+                    next();
+                });
+        }).catch(err => {
+            if (['token expired', 'jwt must be provided'].includes(err.message)) {
+                res.redirect('/login');
                 return;
-            });
+            }
+            next(err);
+        });
 
-        req.user = await usersService.getByIdAsync(userData.data.id);
-        next();
+
+        // const userData = await jwt
+        //     .verifyToken(token)
+        //     .catch(err => {
+        //         if ([
+        //             'token expired',
+        //             'jwt must be provided'
+        //         ].includes(err.message)
+        //         ) {
+        //             res.redirect('/login');
+        //             return;
+        //         }
+        //         next(err);
+        //     });
+
+        // req.user = await usersService.getByIdAsync(userData.data.id);
+        // next();
     }
 }
 
