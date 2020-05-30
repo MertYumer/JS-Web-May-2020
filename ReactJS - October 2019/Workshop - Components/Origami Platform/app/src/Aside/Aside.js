bf0153c4112670b0ee2019f92e90bfd419153920
Mert Yumer <mertyumer66@gmail.com>
2020-05-31 02:54:54 +0300

    Added links in Aside and Footer components

--- ReactJS - October 2019/Workshop - Components/Origami Platform/app/src/Aside/Aside.js
+++ ReactJS - October 2019/Workshop - Components/Origami Platform/app/src/Aside/Aside.js
@@ -8,9 +8,11 @@ function Aside() {
     return (
         <aside className='Aside'>
             <ul>
-                <Link url='#'>Link 1</Link>
-                <Link url='#'>Link 2</Link>
-                <Link url='#'>Link 3</Link>
+                <Link url='/'>Posts</Link>
+                <Link url='/share'>Share</Link>
+                <Link url='/profile'>Profile</Link>
+                <Link url='/register'>Register</Link>
+                <Link url='/login'>Login</Link>
             </ul>
         </aside>
     );
--- ReactJS - October 2019/Workshop - Components/Origami Platform/app/src/Footer/Footer.js
+++ ReactJS - October 2019/Workshop - Components/Origami Platform/app/src/Footer/Footer.js
@@ -8,9 +8,11 @@ function Footer() {
     return (
         <footer className='Footer'>
             <ul>
-                <Link url='#'>Link 1</Link>
-                <Link url='#'>Link 2</Link>
-                <Link url='#'>Link 3</Link>
+                <Link url='/'>Posts</Link>
+                <Link url='/share'>Share</Link>
+                <Link url='/profile'>Profile</Link>
+                <Link url='/register'>Register</Link>
+                <Link url='/login'>Login</Link>
                 <Link url='#'>
                     <img id='logo' src='/blue-origami-bird-flipped.png' alt='my-app-logo' />
                 </Link>
