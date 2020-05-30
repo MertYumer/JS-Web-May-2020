d5d579fb98b73767c545d60d1769d91c2f1aa733
Mert Yumer <mertyumer66@gmail.com>
2020-05-31 01:51:49 +0300

    Added example for adding entities in the database

--- ReactJS - October 2019/Workshop - Components/Origami Platform/REST API/models/index.js
+++ ReactJS - October 2019/Workshop - Components/Origami Platform/REST API/models/index.js
@@ -2,6 +2,15 @@ const User = require('./User');
 const Origami = require('./Origami');
 const TokenBlacklist = require('./TokenBlacklist');
 
+// User.create({ username: 'John', password: '111111' });
+
+// Origami.create({ description: '### Case was created in error or is for testing purposes and should not be used or available for SLA reporting.', author: '5ed2dc4ab5aed528484ae79d' })
+//     .then((createdOrigami) => {
+//         return Promise.all([
+//             User.updateOne({ _id: '5ed2dc4ab5aed528484ae79d' }, { $push: { posts: createdOrigami } }),
+//         ]);
+//     });
+
 module.exports = {
     User,
     Origami,
